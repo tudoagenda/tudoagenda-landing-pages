42ee0d8b8c07287ad9a5c492b6ce3b7a8ee091ee
Carlos Nardo <c.cesar.nardo@gmail.com>
2024-12-12 18:49:47 -0300

    chore: add responsiveness

--- src/app/automatize-seu-atendimento/components/footer/footer.tsx
+++ src/app/automatize-seu-atendimento/components/footer/footer.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export const FooterComponent = () => {
   return (
-    <footer className="flex w-full p-8 justify-end">
+    <footer className="flex w-full p-8 justify-center md:justify-end">
       <Button variant="link">Termos de serviço</Button>
       <Button variant="link">Política de privacidade</Button>
     </footer>
