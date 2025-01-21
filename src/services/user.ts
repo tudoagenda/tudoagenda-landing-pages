ac65ab656c7749bd0ea74f98f69d77d80066a123
Carlos Nardo <c.cesar.nardo@gmail.com>
2025-01-21 15:02:21 -0300

    fix: fix env var naming

--- src/services/user.ts
+++ src/services/user.ts
@@ -3,17 +3,20 @@ type CreateUserResponse = {
   user: string;
 };
 
-const BASE_URL = process.env.NEXT_PUBLIC_ENVIRONMENT_URL;
+const NEXT_PUBLIC_ENVIRONMENT_URL = process.env.NEXT_PUBLIC_ENVIRONMENT_URL;
 
 export const userService = {
   async createUser(email: string): Promise<CreateUserResponse> {
-    const response = await fetch(`${BASE_URL}/agendabela/api/create-user`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email }),
-    });
+    const response = await fetch(
+      `${NEXT_PUBLIC_ENVIRONMENT_URL}/agendabela/api/create-user`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`HTTP error occurred! status: ${response.status}`);
