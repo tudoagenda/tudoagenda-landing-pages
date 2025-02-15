7de93e20f5c1618178445689c00c24a9c48ee9c3
Carlos Nardo <nardo@Carloss-MacBook-Pro.local>
2025-02-14 21:00:06 -0300

    feat: add amplitude

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,8 +3,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { ReactNode } from "react";
+import * as amplitude from '@amplitude/analytics-browser';
 
 const queryClient = new QueryClient();
+
+amplitude.init('348d225df619e203f2984a37eb723124', { autocapture: true });
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
