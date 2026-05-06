83d0a5e20a9ec66a7fde24fad8580333f7c801f4
miguelofoliveir <miguelofoliveira@gmail.com>
2026-05-06 18:04:50 -0300

    chore: troca container GTM do landing para o novo (Tudo Agenda)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,14 +28,14 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NCLL3WLW');`,
+})(window,document,'script','dataLayer','GTM-TSM8GGL6');`,
           }}
         />
       </head>
       <body>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NCLL3WLW"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TSM8GGL6"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
