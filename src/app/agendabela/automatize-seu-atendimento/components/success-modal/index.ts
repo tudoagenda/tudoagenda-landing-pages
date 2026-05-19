ec66b01e33674654306f518379ee1b77532f2077
miguelofoliveir <miguelofoliveira@gmail.com>
2026-05-18 22:26:43 -0300

    chore(lp): cleanup legacy (about/step/testimonial/success-modal) + trust visual

--- src/app/agendabela/automatize-seu-atendimento/components/success-modal/index.ts
+++
@@ -1,1 +0,0 @@
-export { SuccessModal } from "./success-modal";
--- src/app/agendabela/automatize-seu-atendimento/components/trust/__tests__/trust.test.tsx
+++ src/app/agendabela/automatize-seu-atendimento/components/trust/__tests__/trust.test.tsx
@@ -4,7 +4,7 @@ import { TrustComponent } from "../trust";
 describe("TrustComponent", () => {
   it("should render the TrustComponent", () => {
     render(<TrustComponent />);
-    expect(screen.getByAltText("Santander X logo")).toBeInTheDocument();
-    expect(screen.getByAltText("Founders Club logo")).toBeInTheDocument();
+    expect(screen.getByAltText("Santander X")).toBeInTheDocument();
+    expect(screen.getByAltText("Founders Club")).toBeInTheDocument();
   });
 });
